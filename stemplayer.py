bfd3edf281355bb34c1dd3fa2470b4415451c88c
incub4t0r <dchung0812@gmail.com>
2022-02-22 13:50:22 -0500

    added new mod viewing functionality

--- stemplayer.py
+++ stemplayer.py
@@ -2,7 +2,7 @@ import pyaudio, wave, numpy, time, keyboard, os
 p = pyaudio.PyAudio()
 
 cwd = os.path.dirname(os.path.abspath(__file__))
-songFolder = "OffTheGrid"
+songFolder = "Roses"
 targetDir = os.path.join(cwd, songFolder)
 
 drumsWF = wave.open(os.path.join(targetDir,"drums.wav"), "rb")
@@ -15,21 +15,33 @@ vocalsMod = 1
 bassMod = 1
 otherMod = 1
 
+start = 0
 def incMod(modvar):
     if modvar >= 1:
         modvar = 1
     else:
-        modvar += 0.1
+        modvar += 0.05
     return modvar
+
 def decMod(modvar):
     if modvar <= 0:
         modvar = 0
     else:
-        modvar -= 0.1
+        modvar -= 0.05
     return modvar
 
+def toggleMod(modvar):
+    if modvar >= 1:
+        modvar = 0
+    else:
+        modvar = 1
+    return modvar
+
+def on_key_release(key):
+    print('Released Key %s' % key)
+
 def callback(in_data, frame_count, time_info, status):
-    global drumsMod, vocalsMod, bassMod, otherMod
+    global drumsMod, vocalsMod, bassMod, otherMod, keypress, start
     if keyboard.is_pressed('q'):
         drumsMod = incMod(drumsMod)
     elif keyboard.is_pressed('a'):
@@ -46,6 +58,21 @@ def callback(in_data, frame_count, time_info, status):
         otherMod = incMod(otherMod)
     elif keyboard.is_pressed('f'):
         otherMod = decMod(otherMod)
+    if keyboard.is_pressed('z'):
+        drumsMod = toggleMod(drumsMod)
+    elif keyboard.is_pressed('x'):
+        vocalsMod = toggleMod(vocalsMod)
+    elif keyboard.is_pressed('c'):
+        bassMod = toggleMod(bassMod)
+    elif keyboard.is_pressed('v'):
+        otherMod = toggleMod(otherMod)
+    print("\033[F"*5) # go up 5 lines
+    print(
+        f'drumMod:   {drumsMod:.2f}',
+        f'vocalsMod: {vocalsMod:.2f}',
+        f'bassMod:   {bassMod:.2f}',
+        f'otherMod:  {otherMod:.2f}',
+        sep="\n")
     drums = drumsWF.readframes(frame_count)
     vocals = vocalsWF.readframes(frame_count)
     bass = bassWF.readframes(frame_count)
@@ -67,6 +94,7 @@ stream = p.open(format=p.get_format_from_width(drumsWF.getsampwidth()),
 
 try:
     print("[+] Starting stream...")
+    print("\n"*4, end="") # create space for mod
     stream.start_stream()
     while True:
         if not stream.is_active():
